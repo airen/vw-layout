232a1a0edd76535d375f8e6463c0ec4c6eae0c51
Airen <weihua.lwh@alibaba-inc.com>
2018-03-13 10:24:38 +0800

    add test files

---
+++ test/viewport-units-index.js
@@ -0,0 +1,39 @@
+const postcss = require('postcss');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const plugin = require('../');
+
+function compare(filename, warnings, options) {
+  const actual = fs.readFileSync(
+    path.resolve(__dirname, `viewport-units-fixtures/${filename}/actual.css`),
+    { encoding: 'utf8' }
+  );
+  const expected = fs.readFileSync(
+    path.resolve(__dirname, `viewport-units-fixtures/${filename}/expected.css`),
+    { encoding: 'utf8' }
+  );
+
+  return postcss([plugin(options)])
+    .process(actual)
+    .then((result) => {
+      assert.equal(result.css, expected);
+      if (warnings) {
+        assert.deepEqual(result.warnings().map(item => item.text), warnings);
+      }
+
+      return result;
+    });
+}
+
+describe('postcss-viewport-units', () => {
+  it('should automatically append `content` property', () => compare('default-options'));
+
+  it('should only process `calc` value if `options.onlyCalc` is true', () => compare('given-options', null, { onlyCalc: true }));
+
+  it('should give a warning if there is already a `content` property', () => (
+    compare('with-content', [
+      '\'.hero:after\' already has a \'content\' property, give up to overwrite it.',
+    ])
+  ));
+});
